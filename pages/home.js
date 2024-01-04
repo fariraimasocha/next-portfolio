1f6a46d87da6ae504b244b816ecb79d4515dc4fd
fariraimasocha <114150009+fariraimasocha@users.noreply.github.com>
2024-01-04 17:51:57 +0200

    feat
    try flex

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,9 @@
 export default function home() {
     return <div className="">
-        <h1 className="font-sans">
+        <h1 className="font-sans flex-col-reverse bg-red-700 ">
+            Max
+        </h1>
+        <h1 className="font-sans ">
             Max
         </h1>
     </div>
